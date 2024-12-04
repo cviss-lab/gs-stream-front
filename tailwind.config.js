6310fb5ccd5abd659546e7444af67d3a245f9b3d
Kay Han <kyungwanhan@gmail.com>
2024-12-04 12:13:30 -0500

    refactor: Update ESLint and Prettier configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        "navy-blue": "#1a365d",
-        "dark-blue": "#0f2340",
-        "light-gray": "#f7fafc",
+        'navy-blue': '#1a365d',
+        'dark-blue': '#0f2340',
+        'light-gray': '#f7fafc',
       },
     },
   },
